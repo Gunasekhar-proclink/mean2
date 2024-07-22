3de521312d68aa31488e8f2fa4ab3b33e7a9d7d8
Gunasekhar-proclink <gunasekharreddy.chinamallappagari@proclink.com>
2024-07-22 09:42:43 +0530

    day12 commits

--- DAY-11/weather.js
+++ DAY-11/weather.js
@@ -26,22 +26,26 @@
 //   });
 //   console.log(cityInfo);
  
-const API_KEY = "7ca58932b97a48138df71037241907"
-var cities = [ "chennai" , "madurai" , "erode"]
-const urls = cities.map((x) => `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${x}&aqi=no`) ; 
-async function fetchData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-}
+// const API_KEY = "7ca58932b97a48138df71037241907"
+// var cities = [ "chennai" , "madurai" , "erode"]
+// const urls = cities.map((x) => `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${x}&aqi=no`) ; 
+// async function fetchData(url) {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     return data;
+// }
 
-const answer = Promise.all(urls.map(fetchData));
-answer
-    .then(values => {
-        const temperatures = values.reduce((acc, curr) => {
-            acc[curr?.location?.name] = curr?.current?.temp_c;
-            return acc; 
-        }, {});
-        console.log(temperatures);
-    })
-   
+// const answer = Promise.all(urls.map(fetchData));
+// answer
+//     .then(values => {
+//         const temperatures = values.reduce((acc, curr) => {
+//             acc[curr?.location?.name] = curr?.current?.temp_c;
+//             return acc; 
+//         }, {});
+//         console.log(temperatures);
+//     })
+
+
+// fetch("https://669a424b9ba098ed61fef5da.mockapi.io/Users")
+// .then(res => res.json())
+// .then(data => console.log(data))
